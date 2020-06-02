445e93624ec36458edddd3a9e1b65e62789197ce
Federico Berti <berti@leet.it>
2020-06-02 23:35:36 +0100

    md: fix joypad detection

--- src/main/java/omegadrive/joypad/GenesisJoypad.java
+++ src/main/java/omegadrive/joypad/GenesisJoypad.java
@@ -28,12 +28,13 @@ import omegadrive.input.InputProvider.PlayerNumber;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import static omegadrive.joypad.JoypadProvider.JoypadAction.PRESSED;
 import static omegadrive.joypad.JoypadProvider.JoypadAction.RELEASED;
 import static omegadrive.joypad.JoypadProvider.JoypadButton.*;
 
 /**
  * 6-button controller steps
- *                      Bit 5	Bit 4	Bit 3	Bit 2	Bit 1	Bit 0
+ * Bit 5	Bit 4	Bit 3	Bit 2	Bit 1	Bit 0
  * 1st step (write $40)	    C	B	    Right	Left	Down	Up
  * 2nd step (write $00)	Start	A	    0	    0	    Down	Up
  * 3rd step (write $40)	    C	B	    Right	Left	Down	Up
@@ -41,18 +42,17 @@ import static omegadrive.joypad.JoypadProvider.JoypadButton.*;
  * 5th step (write $40)	    C	B	    Right	Left	Down	Up
  * 6th step (write $00)	Start	A	    0	    0	    0	    0
  * 7th step (write $40)	    C	B	    Mode	X	    Y	    Z
+ * 8th step (write $00)	Start	A	    1	    1	    1	    1
  * <p>
- * https://www.plutiedev.com/controllers
- *
- * TODO sgdk_joytest
  */
 public class GenesisJoypad extends BasePadAdapter {
 
     private static Logger LOG = LogManager.getLogger(GenesisJoypad.class.getSimpleName());
 
-    static int SIX_BUTTON_STEPS = 8;  //7+1
-    static int SIX_BUTTON_START_A_ONLY_STEP = 6;
-    static int SIX_BUTTON_XYZ_STEP = 7;
+    static final int SIX_BUTTON_STEPS = 9;
+    static final int SIX_BUTTON_START_A_ONLY_STEP = 6;
+    static final int SIX_BUTTON_XYZ_STEP = 7;
+    static final int SIX_BUTTON_START_A_FINAL_STEP = 8;
 
     //SGDK needs 0 here, otherwise it is considered a RESET
     long control1 = 0;
@@ -62,8 +62,8 @@ public class GenesisJoypad extends BasePadAdapter {
     int readStep1 = 0;
     int readStep2 = 0;
 
-    boolean asserted1;
-    boolean asserted2;
+    boolean high1;
+    boolean high2;
 
     @Override
     public void init() {
@@ -83,34 +83,54 @@ public class GenesisJoypad extends BasePadAdapter {
     }
 
     public void writeDataRegister1(long data) {
-        asserted1 = (data & 0x40) == 0;
-        boolean reset = data == 0 && readStep1 == 0;
-        readStep1 = reset ? 0 : (readStep1 + 1) % SIX_BUTTON_STEPS;
-//        LOG.info("write p1: asserted : {}, step : {}, data: {}", asserted1, readStep1, data);
+        boolean h1 = (data & 0x40) == 0x40;
+        if (h1 != high1) {
+            readStep1 = (readStep1 + 1) % SIX_BUTTON_STEPS;
+            high1 = h1;
+        }
+//        LOG.info("write p1: high : {}, step : {}, data: {}", high1, readStep1, data);
     }
 
     public void writeDataRegister2(long data) {
-        asserted2 = (data & 0x40) == 0;
-        boolean reset = data == 0 && readStep2 == 0;
-        readStep2 = reset ? 0 : (readStep2 + 1) % SIX_BUTTON_STEPS;
+        boolean h2 = (data & 0x40) == 0x40;
+        if (h2 != high2) {
+            readStep2 = (readStep2 + 1) % SIX_BUTTON_STEPS;
+            high2 = h2;
+        }
+//        LOG.info("write p2: high : {}, step : {}, data: {}", high2, readStep2, data);
     }
 
     public int readDataRegister1() {
-        return readDataRegister(PlayerNumber.P1, p1Type, asserted1, readStep1);
-//        LOG.info("read p1: asserted : {}, step : {}, result: {}", asserted1, readStep1, res);
+        return readDataRegister(PlayerNumber.P1, p1Type, high1, readStep1);
+//        LOG.info("read p1: high : {}, step : {}, result: {}", high1, readStep1, res);
     }
 
     public int readDataRegister2() {
-        return readDataRegister(PlayerNumber.P2, p2Type, asserted2, readStep2);
+        return readDataRegister(PlayerNumber.P2, p2Type, high2, readStep2);
+//        LOG.info("read p2: high : {}, step : {}, result: {}", high2, readStep2, res);
     }
 
+    //when pressing Mode, a 6 button behaves like a 3btn
+    private boolean isModePressed(PlayerNumber pn) {
+        return getValue(pn, M) == PRESSED.ordinal();
+    }
 
-    private int readDataRegister(PlayerNumber n, JoypadType type, boolean asserted, int readStep) {
+    private int readDataRegister(PlayerNumber n, JoypadType type, boolean high, int readStep) {
         boolean is6Button = type == JoypadType.BUTTON_6;
-        if (asserted) {
-            return is6Button && readStep == SIX_BUTTON_START_A_ONLY_STEP ? get00SA0000(n) : get00SA00DU(n);
+        if (!is6Button || isModePressed(n)) {
+            return high ? get11CBRLDU(n) : get00SA00DU(n);
+        }
+        if (!high) {
+            switch (readStep) {
+                case SIX_BUTTON_START_A_FINAL_STEP:
+                    return get00SA1111(n);
+                case SIX_BUTTON_START_A_ONLY_STEP:
+                    return get00SA0000(n);
+                default:
+                    return get00SA00DU(n);
+            }
         } else {
-            return is6Button && readStep == SIX_BUTTON_XYZ_STEP ? get11CBMXYZ(n) : get11CBRLDU(n);
+            return readStep == SIX_BUTTON_XYZ_STEP ? get11CBMXYZ(n) : get11CBRLDU(n);
         }
     }
 
@@ -138,6 +158,10 @@ public class GenesisJoypad extends BasePadAdapter {
 
     private int get00SA0000(PlayerNumber n) {
         return (getValue(n, S) << 5) | (getValue(n, A) << 4);
+    }
+
+    private int get00SA1111(PlayerNumber n) {
+        return (getValue(n, S) << 5) | (getValue(n, A) << 4) | 0xF;
     }
 
     //6 buttons
@@ -173,8 +197,8 @@ public class GenesisJoypad extends BasePadAdapter {
 
     @Override
     public void newFrame() {
-        readStep1 = 0;
-        readStep2 = 0;
+        readStep1 = p1Type == JoypadType.BUTTON_6 && high1 ? 1 : 0;
+        readStep2 = p2Type == JoypadType.BUTTON_6 && high2 ? 1 : 0;
 //        LOG.info("new frame");
     }
 }
