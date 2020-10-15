dd70aeb8c1654693ced1b1f8e4f487d30e23fd5d
Federico Berti <berti@leet.it>
2020-10-15 17:54:00 +0100

    minor

--- src/main/java/omegadrive/sound/javasound/JavaSoundManager.java
+++ src/main/java/omegadrive/sound/javasound/JavaSoundManager.java
@@ -90,7 +90,6 @@ public class JavaSoundManager extends AbstractSoundManager {
         return fmBufferLenStereo;
     }
 
-    //TODO broken
     private Runnable getRunnable() {
         return () -> {
             //this needs to be less than one frame worth (ie. < 16.67 ms)
