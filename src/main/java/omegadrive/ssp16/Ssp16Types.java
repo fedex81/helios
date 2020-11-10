5ca6fd49044dc57715c75654a7f8630a073e4109
Federico Berti <berti@leet.it>
2020-11-10 18:02:32 +0000

    oops

--- src/main/java/omegadrive/ssp16/Ssp16Types.java
+++ src/main/java/omegadrive/ssp16/Ssp16Types.java
@@ -107,7 +107,7 @@ public interface Ssp16Types {
             return gr[reg.ordinal()].h;
         }
 
-        static class mem implements Serializable {
+        class mem implements Serializable {
 
             bank bank = new bank();
 
@@ -134,7 +134,7 @@ public interface Ssp16Types {
             }
         }
 
-        public static class ptr implements Serializable {
+        public class ptr implements Serializable {
             final static int REGS_PER_BANK = 4;
             public bank bank = new bank();
 
